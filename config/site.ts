19acb10097a154d629ba6464967c70969b0df6af
Santiago de la cruz Martinez Lara <42554588+PacifiK2460@users.noreply.github.com>
2024-02-22 17:40:17 +0000

    First

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Santiago Lara",
+  description:
+    "I am a hyper-curious and lifelong learner who enjoys working with a variety of technologies and tools. I have experience in Linux, Docker, NextJS, Python, C, Bash, and Rust, and I am always eager to learn new skills and explore new domains. I love to solve problems and help others with my coding abilities, and I have participated in several coding competitions and hackathons. I also have a lot of attention to detail, which helps me ensure the quality and accuracy of my work. I am looking for opportunities to collaborate with other passionate developers and create innovative and impactful solutions.",
+  links: {
+    github: "https://github.com/nextui-org/nextui",
+    twitter: "https://twitter.com/getnextui",
+    docs: "https://nextui.org",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
+};
